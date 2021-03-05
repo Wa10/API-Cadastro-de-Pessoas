f12ca9b31b1581aee3e5cd3b53c4d8d74f9d8071
backdeveloper <walace.alves10@gmail.com>
2021-03-05 18:08:34 -0300

    first commit

---
+++ src/main/java/com/softcadastro/softcadastro/SoftcadastroApplication.java
@@ -0,0 +1,13 @@
+package com.softcadastro.softcadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftcadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftcadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softcadastro/softcadastro/SoftcadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softcadastro.softcadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftcadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
